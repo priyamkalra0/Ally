ff8ccadcd82760300290fb1c31d6ab32ee1b2dd3
priyamkalra0 <priyamkalra0@gmail.com>
2024-05-09 16:40:25 +0530

    ally: imported dotnet project

---
+++ Ally.cs
@@ -0,0 +1,115 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ally
+{
+    public readonly struct Alias
+    {
+        public readonly string Name { get; }
+        public readonly string Value { get; }
+
+        public Alias(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    public static class Ally
+    {
+        private static readonly string DataDirectory = Environment.GetEnvironmentVariable("APPDATA") + @"\Ally";
+
+        static Ally()
+        {
+            // Ensure that the `Ally` data directory exists.
+            Directory.CreateDirectory(DataDirectory);
+        }
+
+        // Register a given `Alias` instance.
+        public static void RegisterAlias(Alias alias) =>
+            CreateAliasFile(
+                GetAliasFilePath(alias.Name),
+                DumpAliasIntoFileBuilder(alias.Value)
+            );
+
+        // Deletes an alias given it's name.
+        public static void DeleteAlias(string name) =>
+            DeleteAliasFile(GetAliasFilePath(name));
+
+        // Clears all registered aliases.
+        public static void ClearAliases()
+        {
+            foreach (Alias alias in IterAliases())
+                DeleteAlias(alias.Name);
+        }
+
+        // Yields `Alias` instances which contain given query in their names.
+        public static IEnumerable<Alias> GetAliases(string query) => 
+            IterAliases(name => name.Contains(query));
+
+        // Yields required `Alias` instances, filtering as needed.
+        public static IEnumerable<Alias> IterAliases(Func<string, bool>? filter = null)
+        {
+            return
+                from file in IterAliasFiles(filter)
+                select LoadAliasFromFile(file);
+        }
+
+        // Returns an `Alias` instance from it's name.
+        public static Alias GetAlias(string name) => LoadAliasFromFile(GetAliasFilePath(name));
+
+        // Prepares the contents required to build an alias (.cmd) file for a given `Alias` instance.
+        private static string[] DumpAliasIntoFileBuilder(string value)
+        {
+            bool explicitDisableParamFwd = value.EndsWith(" %!");
+            if (explicitDisableParamFwd) value = value[..^3];
+
+            // Using parameters manually implies no forwarding.
+            bool implicitDisableParamFwd = new Regex(@"\%[*,0-9]").IsMatch(value);
+
+            string parameterFwdSuffix = (explicitDisableParamFwd || implicitDisableParamFwd) ? "" : " %*";
+
+            return new[] {
+                "@echo off",
+                value.Replace("!%", "%") // "Unescape" enviroment variables
+                + parameterFwdSuffix
+            };
+        }
+
+        // Essentially reverses the dumping process, converting an alias (.cmd) file into an `Alias`.
+        private static Alias LoadAliasFromFile(string path)
+        {
+            string name = new FileInfo(path).Name[..^4];
+            string value = 
+                File.ReadAllLines(path)[^1] // Discard header
+                .Replace("\"", "\\\"") // Escape quotes
+                .Replace("%", "!%"); // "Escape" enviroment variables
+
+
+            bool explicitEnableParamFwd = value.EndsWith(" !%*");
+
+            if (explicitEnableParamFwd) value = value[..^4];
+            else value += " %!";
+
+            return new(name, $"\"{value}\"");
+        }
+
+        // Simply returns the assumed path of an alias (.cmd) file.
+        private static string GetAliasFilePath(string name) => (DataDirectory + @$"\{name}.cmd");
+
+        // FS Wrappers for alias (.cmd) files.
+        private static void DeleteAliasFile(string path) => File.Delete(path);
+        private static void CreateAliasFile(string path, string[] builder) => File.WriteAllLines(path, builder);
+
+        // Yields names of required alias (.cmd) files, filtering as needed.
+        private static IEnumerable<string> IterAliasFiles(Func<string, bool>? filter = null)
+        {
+            IEnumerable<string> files =
+                Directory
+                .EnumerateFiles(DataDirectory)
+                .Where(name => name.EndsWith(".cmd"));
+
+             if (filter == null) return files;
+            return files.Where(filter);
+        }
+    }
+}
---
+++ CLI.cs
@@ -0,0 +1,69 @@
+﻿using System.CommandLine;
+
+namespace Ally
+{
+    internal static class CLI
+    {
+        private const string Description = 
+            "Simple tool for managing unix-like aliases on windows." +
+            "\n  Without arguments, `ally` prints the list of aliases in the reusable form `ally <name> <value>` on standard output." +
+            "\n  Otherwise, if <value> is given, an alias is defined for <name> and <value>," +
+            "\n  and if <value> is not given, any existing alias corresponding to <name> is removed." +
+            "\n  By default, all parameters given when calling alias are forwaded to <value>." +
+            "\n  To disable parameters forwarding, append %! at the end of <value> when defining the alias." +
+            "\n  Additionally, you may use a preceding ! to escape enviroment variables in aliases." +
+            "\n  Ex. ally show-profile \"echo !%USERPROFILE!%\"" +
+            "\n  Now, the enviroment variable will be evaluated when the alias is called."
+            ;
+
+        private static async Task<int> Main(string[] args)
+        {
+            Argument<string?> argName = new(
+                name: "name",
+                description: "The alias to be defined for given value.",
+                getDefaultValue: () => null
+            );
+            Argument<string?> argValue = new(
+                name: "value",
+                description: "Required value to be binded to given alias.",
+                getDefaultValue: () => null
+            );
+
+            Option<string> optSearch = new(
+                name: "--search",
+                description: "Display all aliases that contain <query> in the reusable form `ally <name> <value>`"
+             ) { ArgumentHelpName = "query" };
+            optSearch.AddAlias("-s");
+
+            Option<bool> flagClear = new(
+                name: "--clear",
+                description: "Clear all currently set aliases."
+            );
+            flagClear.AddAlias("-c");
+
+            RootCommand console = new(Description) {
+                argName, 
+                argValue,
+                optSearch,
+                flagClear
+            };
+
+            console.SetHandler(Handler, argName, argValue, optSearch, flagClear);
+            
+            return await console.InvokeAsync(args);
+        }
+
+        private static void Handler(string? name, string? value, string? query, bool clear)
+        {
+            if (clear == true) Ally.ClearAliases(); // Clear
+            else if (query != null) DisplayAliases(Ally.GetAliases(query)); // Search
+            else if (name == null) DisplayAliases(Ally.IterAliases()); // Display
+            else if (value == null) Ally.DeleteAlias(name); // Delete
+            else Ally.RegisterAlias(new(name, value)); // Register
+        }
+
+        private static void DisplayAliases(IEnumerable<Alias> aliases) {
+            foreach (Alias alias in aliases) Console.WriteLine($"ally {alias.Name} {alias.Value}");
+        }
+    }
+}
